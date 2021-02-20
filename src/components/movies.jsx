986cdbbca2c0ac3372971be06a5adf6bbb34c808
adithyaGowda <adithyagowda5696@gmail.com>
2021-02-20 15:40:13 +0530

    adding basic react example

---
+++ src/components/movies.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  render() {
+    const { length: count } = this.state.movies;
+
+    if (count === 0) return <h5>There are no movies in the database.</h5>;
+
+    return (
+      <>
+        <h5>Showing {this.state.movies.length} movies in the database.</h5>
+        <hr />
+        <table className="table table-dark">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => {
+              return (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <button
+                      type="button"
+                      className="btn btn-danger btn-sm"
+                      onClick={() => this.handleDelete(movie)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
+
+export default Movies;
