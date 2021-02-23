112a51751aec2f327f8a7c3af2f33a1940b24716
adithyaGowda <adithyagowda5696@gmail.com>
2021-02-23 12:18:09 +0530

    like toggle

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 
-//INput: liked: boolean
-//Output: onClick
-
-class Like extends Component {
-  state = {};
-  render() {
-    return <i className="fa fa-heart-o" aria-hidden="true"></i>;
-  }
+const Like = (props) => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
+  return (
+    <i
+      onClick={props.onClick}
+      style={{ cursor: "pointer" }}
+      className={classes}
+      aria-hidden="true"
+    ></i>
+  );
 }
-
+ 
 export default Like;
