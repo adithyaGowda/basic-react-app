be2b41c3792780fba0af730f6c7c1346824018f2
adithyaGowda <adithyagowda5696@gmail.com>
2021-02-23 10:34:10 +0530

    like component added

---
+++ src/components/common/like.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+//INput: liked: boolean
+//Output: onClick
+
+class Like extends Component {
+  state = {};
+  render() {
+    return <i className="fa fa-heart-o" aria-hidden="true"></i>;
+  }
+}
+
+export default Like;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
 
 class Movies extends Component {
   state = {
@@ -28,6 +29,7 @@ class Movies extends Component {
               <th scope="col">Stock</th>
               <th scope="col">Rate</th>
               <th scope="col"></th>
+              <th scope="col"></th>
             </tr>
           </thead>
           <tbody>
@@ -38,6 +40,9 @@ class Movies extends Component {
                   <td>{movie.genre.name}</td>
                   <td>{movie.numberInStock}</td>
                   <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <Like />
+                  </td>
                   <td>
                     <button
                       type="button"
