2b26ef460d3b9aeb934f62971044374199ab2ad9
adithyaGowda <adithyagowda5696@gmail.com>
2021-04-06 12:26:00 +0530

    extracting TableBody

---
+++ src/components/common/tableBody.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class TableBody extends Component {
+  render() {
+    const { data, columns } = this.props;
+    return (
+      <tbody>
+        {data.map((item) => {
+          return (
+            <tr>
+              {columns.map((column) => {
+                return <td></td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    );
+  }
+}
+
+export default TableBody;
