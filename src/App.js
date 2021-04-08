919a3086cbfca676eb01bf1045904537fe857fcb
adithyaGowda <adithyagowda5696@gmail.com>
2021-04-08 19:26:52 +0530

    added navBar and implemented routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import './App.css';
+import { Route, Redirect, Switch } from "react-router-dom";
+import NavBar from "./components/navbar";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rental";
+import NotFound from "./components/notFound";
+import "./App.css";
 
 function App() {
   return (
-    <main className= "container">
-      <Movies/>
-    </main>
-    
+    <>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </>
   );
 }
 
