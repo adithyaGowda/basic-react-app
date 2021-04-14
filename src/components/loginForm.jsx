ce6b1ca4f479b9e07f8c11ef303b2d6bd05880b7
adithyaGowda <adithyagowda5696@gmail.com>
2021-04-14 19:42:04 +0530

    extracting a reusable input

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Input from "./common/input";
 
 class LoginForm extends Component {
   state = {
@@ -24,29 +25,18 @@ class LoginForm extends Component {
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username"></label>Username
-            <input
-              autoFocus
-              name="username"
-              id="username"
-              type="text"
-              className="form-control"
-              value={account.username}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password"></label>Password
-            <input
-              name="password"
-              value={account.password}
-              id="password"
-              type="text"
-              className="form-control"
-              onChange={this.handleChange}
-            />
-          </div>
+          <Input
+            name="username"
+            label="Username"
+            value={account.username}
+            onChange={this.handleChange}
+          />
+          <Input
+            name="password"
+            label="Password"
+            value={account.password}
+            onChange={this.handleChange}
+          />
           <button className="btn btn-primary">Login</button>
         </form>
       </div>
