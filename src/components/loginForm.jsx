2c360153e9ee125ee2a6598330d3ed386f9766fb
adithyaGowda <adithyagowda5696@gmail.com>
2021-04-15 08:03:14 +0530

    validation

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -4,13 +4,21 @@ import Input from "./common/input";
 class LoginForm extends Component {
   state = {
     account: { username: "", password: "" },
+    errors: {},
+  };
+
+  validate = () => {
+    return { username: "Username is required" };
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
 
-    const username = this.username.current.value;
-    console.log("submitted ", username);
+    const errors = this.validate();
+    this.setState({ errors });
+    if (errors) return;
+
+    console.log("submitted");
   };
 
   handleChange = ({ currentTarget: input }) => {
