eb2a729d9a56eab15cf8dc036420c022cd88914b
adithyaGowda <adithyagowda5696@gmail.com>
2021-05-15 10:23:53 +0530

    submitting the registration form

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Form from "./common/form";
 import Joi from "joi-browser";
+import * as userService from "../services/userService";
 
 class RegisterForm extends Form {
   state = {
@@ -13,9 +14,8 @@ class RegisterForm extends Form {
     name: Joi.string().required().label("Name"),
   };
 
-  doSubmit = () => {
-    //call the server
-    console.log("Submitted");
+  doSubmit = async () => {
+    await userService.register(this.state.data);
   };
 
   render() {
