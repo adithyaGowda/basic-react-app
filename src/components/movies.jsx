f56185ee52b00fc7d925d6a0e7505d5cba05d210
adithyaGowda <adithyagowda5696@gmail.com>
2021-05-15 21:53:24 +0530

    showing or hiding elements based on the user

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -96,6 +96,7 @@ class Movies extends Component {
   render() {
     const { length: count } = this.state.movies;
     const { pageSize, currentPage, sortColumn, searchQuery } = this.state;
+    const { user } = this.props;
 
     if (count === 0) return <h5>There are no movies in the database.</h5>;
 
@@ -111,13 +112,15 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <Link
-            to="/movies/new"
-            className="btn btn-primary"
-            style={{ marginBottom: 20 }}
-          >
-            New Movie
-          </Link>
+          {user && (
+            <Link
+              to="/movies/new"
+              className="btn btn-primary"
+              style={{ marginBottom: 20 }}
+            >
+              New Movie
+            </Link>
+          )}
           <h5>Showing {totalCount} movies in the database.</h5>
           <SearchBox value={searchQuery} onChange={this.handleSearch} />
           <MoviesTable
